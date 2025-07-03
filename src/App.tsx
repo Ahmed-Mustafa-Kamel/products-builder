8a86af7dd1f1f5d3a03ceb41bfe123261c571192
Ahmed Mustafa Kamel <ahmed.kamel.dev@gmail.com>
2025-07-03 17:50:40 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
-import ProductCard from "./components/ProductCard";
-import { productList } from "./data";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const renderProductList = productList.map((product) => (
-    <ProductCard key={product.id} product={product} />
-  ));
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section className="grid grid-cols-1 gap-4 p-8 md:p-16 lg:p-24 md:grid-cols-2 lg:grid-cols-4 ">
-        {renderProductList}
-      </section>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
